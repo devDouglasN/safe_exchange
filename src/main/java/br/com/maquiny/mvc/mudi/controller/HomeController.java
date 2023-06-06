a3ad13ee536318f0d563fbe2b8b4b2162365e1e0
Douglas <douglas_timaofiel@hotmail.com>
2023-06-06 18:44:36 -0300

    Implement filtering orders by status

--- src/main/java/br/com/maquiny/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/maquiny/mvc/mudi/controller/HomeController.java
@@ -6,23 +6,47 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.maquiny.mvc.mudi.model.Pedido;
+import br.com.maquiny.mvc.mudi.model.StatusPedido;
 import br.com.maquiny.mvc.mudi.repository.PedidoRepository;
 
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
 
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
-
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
 		return "home";
+	}
+	
+	@GetMapping("/aguardando")
+	public String aguardando(Model model) {
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.AGUARDANDO);
+		model.addAttribute("pedidos", pedidos);
+		return "home";
+	}
+	
+	@GetMapping("/{status}")
+	public String porStatus(@PathVariable("status") String status, Model model) {
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+		model.addAttribute("pedidos", pedidos);
+		model.addAttribute("status", status);
+		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
 	}
 }
--- src/main/java/br/com/maquiny/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/maquiny/mvc/mudi/repository/PedidoRepository.java
@@ -1,12 +1,17 @@
 package br.com.maquiny.mvc.mudi.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.maquiny.mvc.mudi.model.Pedido;
+import br.com.maquiny.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
 
 }
