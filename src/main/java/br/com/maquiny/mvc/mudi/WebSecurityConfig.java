ca583655b931fc109d71750c7d511bc3f3c6fb10
Douglas <douglas_timaofiel@hotmail.com>
2023-06-12 10:01:54 -0300

    Implementation of user authentication and security configuration using JDBC Authentication

--- src/main/java/br/com/maquiny/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/maquiny/mvc/mudi/WebSecurityConfig.java
@@ -1,24 +1,20 @@
 package br.com.maquiny.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
 	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
+	@Autowired
+    private DataSource dataSource;
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -28,20 +24,29 @@ public class WebSecurityConfig {
 	        )
 	    	.formLogin(form -> form
 	    			.loginPage("/login")
+	    			.defaultSuccessUrl("/home", true)
 	    			.permitAll()
 	    		)
 	    	.logout(logout -> logout.logoutUrl("/logout"));     
 	    return http.build();
 	}
-	 
-	@Bean
-	public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
-	    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-	    manager.createUser(User.withUsername("douglas")
-	        .password(bCryptPasswordEncoder.encode("1"))
-	        .roles("ADM")
-	        .build());
 	
-	    return manager;
-	}
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username, authority FROM authorities WHERE username = ?")
+                .passwordEncoder(encoder);
+        
+        // Criando usuário e senha no bando de dados
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder)
+                .withUser("douglas")
+                .password(encoder.encode("123"))
+                .roles("ADM");
+    }
 }
