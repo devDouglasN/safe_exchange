037cf0118d896848b64c568f2311fc0e02ecac54
Douglas <douglas_timaofiel@hotmail.com>
2023-06-22 11:52:13 -0300

    Implemented user order viewing screen by status

--- src/main/java/br/com/maquiny/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/maquiny/mvc/mudi/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig {
 	        )
 	    	.formLogin(form -> form
 	    			.loginPage("/login")
-	    			.defaultSuccessUrl("/home", true)
+	    			.defaultSuccessUrl("/usuario/pedido", true)
 	    			.permitAll()
 	    		)
 	    	.logout(logout -> logout.logoutUrl("/logout"))   
--- src/main/java/br/com/maquiny/mvc/mudi/controller/UsuarioController.java
+++ src/main/java/br/com/maquiny/mvc/mudi/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package br.com.maquiny.mvc.mudi.controller;
 
-
 import java.security.Principal;
 import java.util.List;
 
@@ -16,31 +15,31 @@ import br.com.maquiny.mvc.mudi.model.Pedido;
 import br.com.maquiny.mvc.mudi.model.StatusPedido;
 import br.com.maquiny.mvc.mudi.repository.PedidoRepository;
 
-
 @Controller
-@RequestMapping("/home")
-public class HomeController {
-
+@RequestMapping("usuario")
+public class UsuarioController {
+	
 	@Autowired
 	private PedidoRepository pedidoRepository;
-
-	@GetMapping
+	
+	@GetMapping("pedido")
 	public String home(Model model, Principal principal) {
-		List<Pedido> pedidos = pedidoRepository.findAll();
+		List<Pedido> pedidos = pedidoRepository.findAllByUsuario(principal.getName());
 		model.addAttribute("pedidos", pedidos);
-		return "home";
+		return "usuario/home";
 	}
 	
-	@GetMapping("/{status}")
-	public String porStatus(@PathVariable("status") String status, Model model) {
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+	@GetMapping("pedido/{status}")
+	public String porStatus(@PathVariable("status") String status, Model model, Principal principal) {
+		List<Pedido> pedidos = pedidoRepository.findByStatusEUsuario (StatusPedido.valueOf(status.toUpperCase()), principal.getName());
 		model.addAttribute("pedidos", pedidos);
 		model.addAttribute("status", status);
-		return "home";
+		return "usuario/home";
 	}
 	
 	@ExceptionHandler(IllegalArgumentException.class)
-	public String onError() {
-		return "redirect:/home";
+	public String onError(){
+		return "redirect:/usuario/home";
 	}
+	
 }
