6eabbd69f80e69842ce561ceb879007fc8edba13
Douglas <douglas_timaofiel@hotmail.com>
2023-06-26 20:48:02 -0300

    Implement sorting and pagination of orders on the home page

--- src/main/java/br/com/maquiny/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/maquiny/mvc/mudi/controller/HomeController.java
@@ -5,6 +5,8 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,7 +28,10 @@ public class HomeController {
 
 	@GetMapping
 	public String home(Model model, Principal principal) {
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0, 1, sort);
+		
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
--- src/main/java/br/com/maquiny/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/maquiny/mvc/mudi/repository/PedidoRepository.java
@@ -3,6 +3,7 @@ package br.com.maquiny.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +15,7 @@ import br.com.maquiny.mvc.mudi.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-	List<Pedido> findByStatus(StatusPedido status);
+	List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 	
 	@Query("select p from Pedido p join p.user u where u.username = :username")
 	List<Pedido> findAllByUsuario(@Param("username")String username);
