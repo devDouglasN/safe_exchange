a43e30a8cab416004bd1af9e2c6ca5ee616ef190
Douglas <douglas_timaofiel@hotmail.com>
2023-07-05 19:59:15 -0300

    Adding data validation and exception handling to offer creation

--- src/main/java/br/com/maquiny/mvc/mudi/dto/RequisicaoNovaOferta.java
+++ src/main/java/br/com/maquiny/mvc/mudi/dto/RequisicaoNovaOferta.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import br.com.maquiny.mvc.mudi.model.Oferta;
 
 public class RequisicaoNovaOferta {
@@ -11,8 +14,15 @@ public class RequisicaoNovaOferta {
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	private Long pedidoId;
+	
+	@Pattern(regexp = "^\\d+(\\.\\d+{2})?$")
+	@NotNull
 	private String valor;
+	
+	@Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
+	@NotNull
 	private String dataDaEntrega;
+	
 	private String comentario;
 	
 	public Long getPedidoId() {
